06060c96c362cc50660ece44ffe8e895af92d320
Nath <55251304+candiath@users.noreply.github.com>
2024-08-26 16:14:44 -0300

    Clase 273

--- src/config/jwt.adapter.ts
+++ src/config/jwt.adapter.ts
@@ -1,5 +1,7 @@
 import jwt from 'jsonwebtoken';
-import { resolve } from 'path';
+import { envs } from './envs';
+
+const JWT_SEED = envs.JWT_SEED;
 
 
 
@@ -10,7 +12,7 @@ export class JwtAdapter {
     static generateToken( payload: any, duration: string = '2h' ) {
 
         return new Promise( (resolve) => {
-            jwt.sign(payload, "SEED", { expiresIn: duration }, (err, token) => {
+            jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (err, token) => {
 
                 if ( err ) return resolve(null); 
 
